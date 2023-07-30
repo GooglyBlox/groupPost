ac1fb728fefb4beaacb658dfe454083aa897b9a9
Google <53668973+GooglyBlox@users.noreply.github.com>
2023-07-30 04:22:16 -0700

    Update background.js

--- src/background.js
+++ src/background.js
@@ -1,22 +1,19 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request in background script:", request); // Debugging line
-  
-    if (request.action === 'postTweet') {
-      // Retrieve the saved webhooks
-      chrome.storage.sync.get('webhooks', function(data) {
-        var webhooks = data.webhooks || [];
-  
-        // Send the tweet URL to each webhook
-        webhooks.forEach(function(webhook) {
-          fetch(webhook, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ content: request.url })
-          });
+  console.log("Received request in background script:", request);
+
+  if (request.action === 'postTweet' || request.action === 'postInstagram') {
+    chrome.storage.sync.get('webhooks', function(data) {
+      var webhooks = data.webhooks || [];
+
+      webhooks.forEach(function(webhook) {
+        fetch(webhook, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ content: request.url })
         });
-  
-        sendResponse({ success: true }); // Response to content script
       });
-    }
-  });
-  
+
+      sendResponse({ success: true });
+    });
+  }
+});
