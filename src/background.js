d73bc5cc131f467b5607146aabbb17a4fffd2008
Google <53668973+GooglyBlox@users.noreply.github.com>
2023-08-10 06:04:12 -0700

    Clipboard security

--- src/background.js
+++ src/background.js
@@ -1,7 +1,9 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   console.log("Received request in background script:", request);
 
-  if (request.action === 'postTweet' || request.action === 'postInstagram' || request.action === 'postTikTok') {
+  if ((request.action === 'postTweet' || request.action === 'postInstagram' || request.action === 'postTikTok') &&
+      (request.url.includes('twitter.com') || request.url.includes('instagram.com') || request.url.includes('tiktok.com'))) {
+    
     chrome.storage.sync.get('webhooks', function(data) {
       var webhooks = data.webhooks || [];
 
